a2798f51c3861b9b527c8f7107a9837d3b2dc23e
esjo <anderson93@naver.com>
2022-04-01 02:33:07 +0900

    solved 10844 in top-down method

--- dp/esjo_10844.cpp
+++ dp/esjo_10844.cpp
@@ -1,3 +1,4 @@
+// 1) Top-Down Method
 #include <iostream>
 #include <vector>
 
@@ -11,33 +12,84 @@ bool check(int i){
     else return true;
 }
 
+int dp(int m, int n){
+    if(cache[m][n] != -1) return cache[m][n];
+    
+    cache[m][n] = 0;
+    if(check(n-1)) cache[m][n] += dp(m-1, n-1);
+    if(check(n+1)) cache[m][n] += dp(m-1, n+1);
+    
+    cache[m][n] %= 1000000000;
+    return cache[m][n];
+}
+
 int dp(int n){
     int ans = 0;
 
-    for(int i = 1 ; i < 10; i++)
+    cache[0][0] = 0;
+    for(int i = 1 ; i < 10 ; i++)
         cache[0][i] = 1;
-    
-    for(int i = 1 ; i < n ; i++)
-        for(int j = 0; j < 10; j++){
-            if(check(j-1)) cache[i][j] += cache[i-1][j-1];
-            if(check(j+1)) cache[i][j] += cache[i-1][j+1];
-            cache[i][j] %= 1000000000;
-        }
 
-    for(auto it = cache[n-1].begin() ; it != cache[n-1].end() ; it++){
-        ans += *it;
+    for(int i = 0 ; i < 10 ; i++){
+        ans += dp(n-1, i);
         ans %= 1000000000;
     }
-    
+
     return ans;
 }
 
 
 int main (){
     cin >> n;
-    cache = vector<vector<int> >(n, vector<int>(10, 0));
+    cache = vector<vector<int> >(n, vector<int>(10, -1));
     
     cout << dp(n);    
 
     return 0;
 }
+
+// 2) Bottop-Up Method
+
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// int n;
+// vector<vector<int> > cache;
+
+// bool check(int i){
+//     if (i < 0 || i > 9) return false;
+//     else return true;
+// }
+
+// int dp(int n){
+//     int ans = 0;
+
+//     for(int i = 1 ; i < 10; i++)
+//         cache[0][i] = 1;
+    
+//     for(int i = 1 ; i < n ; i++)
+//         for(int j = 0; j < 10; j++){
+//             if(check(j-1)) cache[i][j] += cache[i-1][j-1];
+//             if(check(j+1)) cache[i][j] += cache[i-1][j+1];
+//             cache[i][j] %= 1000000000;
+//         }
+
+//     for(auto it = cache[n-1].begin() ; it != cache[n-1].end() ; it++){
+//         ans += *it;
+//         ans %= 1000000000;
+//     }
+    
+//     return ans;
+// }
+
+
+// int main (){
+//     cin >> n;
+//     cache = vector<vector<int> >(n, vector<int>(10, 0));
+    
+//     cout << dp(n);    
+
+//     return 0;
+// }
